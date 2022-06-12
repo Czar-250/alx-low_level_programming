81c4661dcab2d345163ccccb12c41ad704a0f7c5
Czar-250 <manziczar@gmail.com>
2022-06-12 12:32:41 -0700

    Alphabets count

--- 1-alphabet.c
+++ 1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
+
 /**
 *print_alphabet - check the code ALX School students.
 *Description: Prints the alphabet with _putchar
 *Return: Always 0.
 */
-
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(c);
+	_putchar(aphabet);
 	}
 	_putchar('\n');
 }
