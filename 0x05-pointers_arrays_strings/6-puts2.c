08c8b67a05dde25969219b277e0306218a2548a8
Czar-250 <manziczar@gmail.com>
2022-06-14 20:09:05 -0700

    7,8,9 Tasks

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* puts2 - prints other character of a string
+* starting with the first chaaracter
+* Return: void
+* @str: string to be printed
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
