cdfd92a6eda74b4cd3d2d58d957b15ce1ff2c395
Czar-250 <manziczar@gmail.com>
2022-06-28 15:46:50 -0700

    0-4 Malloc Tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* create_array - creates an array of chars,
+* and initializes it with a specific char
+* @c: char to initialize the array
+* @size: size of an array to create
+* Return: NULL if size = 0 || a pointer to the array or NULL if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	p = (char *)malloc(sizeof(char) * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* *_strdup - returns a pointer to a newly allocated space in memory,
+* which contains a copy of the string given as a parameter.
+* @str: string to duplicate
+*Return: NULL if str = NULL || if insufficient memory was available
+* or a pointer to the duplicated string (On success)
+*/
+char *_strdup(char *str)
+{
+	char *a;
+
+	int i, len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len])
+		len++;
+
+	a = (char *) malloc(sizeof(char) * (len + 1));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		a[i] = str[i];
+	}
+	return (a);
+}
